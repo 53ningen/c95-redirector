7c2187eb8e4224f677c040cb2c5b7d11042aba9d
gomi_ningen <gomi2ngen@gmail.com>
2018-11-16 03:55:14 +0900

    徳島関連のURLを追加

--- app.py
+++ app.py
@@ -19,3 +19,18 @@ def kumamoto_cityTramTimeTables():
     url = 'http://www.kotsu-kumamoto.jp/timetable/pub/rosen.aspx?c_id=42'
     return getRedirectResponse(url)
 
+@app.route('/tokushima/airportbus-timetables')
+def tokushima_airportBusTimeTables():
+    url = 'https://www.tokushima-airport.co.jp/access/bus/'
+    return getRedirectResponse(url)
+
+@app.route('/tokushima/naruto-timetables')
+def tokushima_narutoTimeTables():
+    url = 'http://www.jr-shikoku.co.jp/01_trainbus/kakueki/naruto/'
+    return getRedirectResponse(url)
+
+@app.route('/tokushima/bus-timetables')
+def tokushima_busTimeTables():
+    url = 'http://tokubus.co.jp/wptbc/routebus/'
+    return getRedirectResponse(url)
+
